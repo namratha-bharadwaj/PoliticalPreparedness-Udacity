7e56a35f3ecfbaeb3543e4b780e827f2e5b87220
Namratha Bharadwaj <nbharadwaj@paypal.com>
2022-10-26 15:52:44 -0700

    add animation to the representatives list

--- political-preparedness/app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
+++ political-preparedness/app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentLaunchBinding
 
 class LaunchFragment : Fragment() {
